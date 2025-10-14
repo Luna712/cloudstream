f39c44f37144740f1be4976f6a549a44978d4982
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-10-14 17:48:14 -0600

    Update UIHelper.kt

--- app/src/main/java/com/lagradost/cloudstream3/utils/UIHelper.kt
+++ app/src/main/java/com/lagradost/cloudstream3/utils/UIHelper.kt
@@ -451,7 +451,7 @@ object UIHelper {
 
                 widthResId?.let {
                     val widthPx = view.resources.getDimensionPixelSize(it)
-                    view.updatePadding(right = 0)
+                    view.updatePadding(right = 0, left = systemBars.left)
                     view.updateLayoutParams {
                         if (systemBars.left > 0) {
                             width = widthPx + systemBars.left
