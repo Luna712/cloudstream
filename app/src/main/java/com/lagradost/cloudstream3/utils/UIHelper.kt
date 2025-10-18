c002dc3b6c209faa26f301f26f379dfdee4d7754
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-10-18 14:34:50 -0600

    Remove unused cutout insets in UIHelper
    Removed top and bottom cutout insets from UIHelper.

--- app/src/main/java/com/lagradost/cloudstream3/utils/UIHelper.kt
+++ app/src/main/java/com/lagradost/cloudstream3/utils/UIHelper.kt
@@ -477,9 +477,7 @@ object UIHelper {
                 val cutout = windowInsets.displayCutout
                 if (cutout != null) {
                     val left = cutout.safeInsetLeft
-                    val top = cutout.safeInsetTop
                     val right = cutout.safeInsetRight
-                    val bottom = cutout.safeInsetBottom
 
                     view.background = object : Drawable() {
                         private val paint = Paint().apply {
@@ -489,7 +487,7 @@ object UIHelper {
                         override fun draw(canvas: Canvas) {
                             if (left > 0) canvas.drawRect(0f, 0f, left.toFloat(), view.height.toFloat(), paint)
                             if (right > 0) canvas.drawRect(
-                                right.toFloat(), 0f,
+                                view.width - right.toFloat(),
                                 view.width.toFloat(),
                                 view.height.toFloat(),
                                 paint
