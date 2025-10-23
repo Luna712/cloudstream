1333c2a1c271620e327f0dd4e4f2175a9a290cf1
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-10-23 16:02:44 -0600

    Update GeneratorPlayer.kt

--- app/src/main/java/com/lagradost/cloudstream3/ui/player/GeneratorPlayer.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/player/GeneratorPlayer.kt
@@ -1065,7 +1065,6 @@ class GeneratorPlayer : FullScreenPlayer() {
                     safe {
                         val subtitlesFragment = SubtitlesFragment()
                         subtitlesFragment.systemBarsAddPadding = true
-                        subtitlesFragment.backgroundColor = R.attr.primaryBlackBackground
                         subtitlesFragment.show(this.parentFragmentManager, "SubtitleSettings")
                     }
                 }
