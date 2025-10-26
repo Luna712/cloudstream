06b5949772176a12b48c2c744d44133aaa1317e1
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-10-26 16:59:56 -0600

    Cleanup

--- app/src/main/java/com/lagradost/cloudstream3/ui/BaseFragment.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/BaseFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
-import androidx.viewbinding.ViewBinding
 import com.lagradost.cloudstream3.ui.settings.Globals.EMULATOR
 import com.lagradost.cloudstream3.ui.settings.Globals.TV
 import com.lagradost.cloudstream3.ui.settings.Globals.isLandscape
@@ -15,49 +14,47 @@ import com.lagradost.cloudstream3.ui.settings.Globals.isLayout
 import com.lagradost.cloudstream3.utils.UIHelper.fixSystemBarsPadding
 
 abstract class BaseFragment<T : ViewBinding>(
-	private val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> T
+    private val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> T
 ) : Fragment() {
 
-	protected var _binding: T? = null
-	protected val binding: T? get() = _binding
+    protected var _binding: T? = null
+    protected val binding: T? get() = _binding
 
-	override fun onCreateView(
-		inflater: LayoutInflater,
-		container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View? {
-		_binding = bindingInflater(inflater, container, false)
-		return _binding?.root
-	}
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflater(inflater, container, false)
+        return _binding?.root
+    }
 
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
-        binding?.let {
-			fixSystemBarsPadding(
-				it.root,
-				padBottom = isLandscape(),
-				padLeft = isLayout(TV or EMULATOR)
-			)
-		}
-		binding?.let { onBindingCreated(it, savedInstanceState) }
-	}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fixPadding()
+        binding?.let { onBindingCreated(it, savedInstanceState) }
+    }
 
-	/** Called when binding has been safely created and view is ready. */
+    /** Called when binding has been safely created and view is ready. */
     protected open fun onBindingCreated(binding: T, savedInstanceState: Bundle?) {}
 
-	override fun onConfigurationChanged(newConfig: Configuration) {
-        binding?.let {
-			fixSystemBarsPadding(
-				it.root,
-				padBottom = isLandscape(),
-				padLeft = isLayout(TV or EMULATOR)
-			)
-		}
-		super.onConfigurationChanged(newConfig)
-	}
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        fixPadding()
+        super.onConfigurationChanged(newConfig)
+    }
 
-	override fun onDestroyView() {
-		super.onDestroyView()
-		_binding = null
-	}
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun fixPadding() {
+        binding?.let {
+            fixSystemBarsPadding(
+                it.root,
+                padBottom = isLandscape(),
+                padLeft = isLayout(TV or EMULATOR)
+            )
+        }
+    }
 }
