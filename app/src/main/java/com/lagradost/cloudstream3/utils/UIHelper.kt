45a00033baf7e407b88234470bec272682e8904f
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-10-26 13:26:20 -0600

    Fix edge to edge issue

--- app/src/main/java/com/lagradost/cloudstream3/utils/UIHelper.kt
+++ app/src/main/java/com/lagradost/cloudstream3/utils/UIHelper.kt
@@ -454,10 +454,10 @@ object UIHelper {
             )
 
             view.updatePadding(
-                left = if (leftCheck) insets.left else view.paddingLeft,
-                right = if (rightCheck) insets.right else view.paddingRight,
-                bottom = if (padBottom) insets.bottom else view.paddingBottom,
-                top = if (padTop) insets.top else view.paddingTop
+                left = if (leftCheck) insets.left else 0,
+                right = if (rightCheck) insets.right else 0,
+                bottom = if (padBottom) insets.bottom else 0,
+                top = if (padTop) insets.top else 0
             )
 
             heightResId?.let {
