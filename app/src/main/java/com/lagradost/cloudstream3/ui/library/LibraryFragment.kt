db7cf7fb5fc45bc5175f10e24a5669ae56764a8d
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-10-28 14:58:40 -0600

    test change

--- app/src/main/java/com/lagradost/cloudstream3/ui/library/LibraryFragment.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/library/LibraryFragment.kt
@@ -540,7 +540,7 @@ class LibraryFragment : BaseFragment<FragmentLibraryBinding>(
 
     @SuppressLint("NotifyDataSetChanged")
     override fun onConfigurationChanged(newConfig: Configuration) {
-        binding?.viewpager?.adapter?.notifyDataSetChanged()
+        // binding?.viewpager?.adapter?.notifyDataSetChanged()
         super.onConfigurationChanged(newConfig)
     }
 
