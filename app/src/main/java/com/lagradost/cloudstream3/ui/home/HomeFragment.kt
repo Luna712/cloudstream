a57df27ca3aec861229b37f685bd3734ebaa8693
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-05 17:49:40 -0700

    Fix

--- app/src/main/java/com/lagradost/cloudstream3/ui/home/HomeFragment.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/home/HomeFragment.kt
@@ -25,7 +25,6 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.chip.Chip
 import com.lagradost.api.Log
-import com.lagradost.cloudstream3.AcraApplication.Companion.getKey
 import com.lagradost.cloudstream3.APIHolder.apis
 import com.lagradost.cloudstream3.AllLanguagesName
 import com.lagradost.cloudstream3.CommonActivity.showToast
@@ -55,7 +54,6 @@ import com.lagradost.cloudstream3.ui.settings.Globals.PHONE
 import com.lagradost.cloudstream3.ui.settings.Globals.TV
 import com.lagradost.cloudstream3.ui.settings.Globals.isLandscape
 import com.lagradost.cloudstream3.ui.settings.Globals.isLayout
-import com.lagradost.cloudstream3.ui.setup.HAS_DONE_SETUP_KEY
 import com.lagradost.cloudstream3.utils.AppContextUtils.filterProviderByPreferredMedia
 import com.lagradost.cloudstream3.utils.AppContextUtils.getApiProviderLangSettings
 import com.lagradost.cloudstream3.utils.AppContextUtils.isNetworkAvailable
@@ -555,10 +553,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(
     override fun pickLayout(): Int? =
         if (isLayout(PHONE)) R.layout.fragment_home else R.layout.fragment_home_tv
 
+    var useBindingPool = true
     override fun getPoolKey(): String {
-        // Setup causes cache to be poisoned, so don't use until
-        // setup is done.
-        if (getKey(HAS_DONE_SETUP_KEY, false) != true) return ""
+        if (!useBindingPool) return ""
         return "HomeFragment"
     }
 
