d30573f18bd99c4803e27a474fd6849c4cc2f3be
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-10 14:17:00 -0700

    Update build.gradle.kts

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -71,6 +71,14 @@ buildkonfig {
     exposeObjectWithName = "BuildConfig"
 
     defaultConfigs {
+        val isDebug = kotlin.runCatching { extra.get("isDebug") }.getOrNull() == true
+        if (isDebug) {
+            logger.quiet("Compiling library with debug flag")
+        } else {
+            logger.quiet("Compiling library with release flag")
+        }
+        buildConfigField(FieldSpec.Type.BOOLEAN, "DEBUG", isDebug.toString())
+
         // Reads local.properties
         val localProperties = gradleLocalProperties(rootDir, project.providers)
         buildConfigField(
