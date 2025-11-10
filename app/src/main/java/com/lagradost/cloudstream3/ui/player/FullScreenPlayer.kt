c23197bc1a77340357c8fc33cbeb3f5deebb64e9
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-10 15:24:02 -0700

    3000

--- app/src/main/java/com/lagradost/cloudstream3/ui/player/FullScreenPlayer.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/player/FullScreenPlayer.kt
@@ -943,7 +943,7 @@ open class FullScreenPlayer : AbstractPlayerFragment() {
             if (!isCurrentTouchValid && isShowing && index == currentTapIndex && player.getIsPlaying()) {
                 onClickChange()
             }
-        }, 5000)
+        }, 3000)
     }
 
     // this is used because you don't want to hide UI when double tap seeking
