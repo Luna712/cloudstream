3b70f2a47717a25933b7fdac23c3f5e7a5d0467b
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-13 16:29:14 -0700

    Update

--- app/src/main/java/com/lagradost/cloudstream3/AcraApplication.kt
+++ app/src/main/java/com/lagradost/cloudstream3/AcraApplication.kt
@@ -11,4 +11,38 @@ package com.lagradost.cloudstream3
     replaceWith = ReplaceWith("com.lagradost.cloudstream3.CloudStreamApp"),
     level = DeprecationLevel.WARNING
 )*/
-class AcraApplication : CloudStreamApp()
+class AcraApplication {
+	companion object {
+		val context get() = CloudStreamApp.context
+
+		fun removeKeys(folder: String): Int? =
+			CloudStreamApp.removeKeys(folder)
+
+		fun <T> setKey(path: String, value: T) =
+			CloudStreamApp.setKey(path, value)
+
+		fun <T> setKey(folder: String, path: String, value: T) =
+			CloudStreamApp.setKey(folder, path, value)
+
+		inline fun <reified T : Any> getKey(path: String, defVal: T?): T? =
+			CloudStreamApp.getKey(path, defVal)
+
+		inline fun <reified T : Any> getKey(path: String): T? =
+			CloudStreamApp.getKey(path)
+
+		inline fun <reified T : Any> getKey(folder: String, path: String): T? =
+			CloudStreamApp.getKey(folder, path)
+
+		inline fun <reified T : Any> getKey(folder: String, path: String, defVal: T?): T? =
+			CloudStreamApp.getKey(folder, path, defVal)
+
+		fun getKeys(folder: String): List<String>? =
+			CloudStreamApp.getKeys(folder)
+
+		fun removeKey(folder: String, path: String) =
+			CloudStreamApp.removeKey(folder, path)
+
+		fun removeKey(path: String) =
+			CloudStreamApp.removeKey(path)
+	}
+}
