8c4cc187e9ca91be260cd47b67ae26865cbfc942
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-21 18:09:03 -0700

    Try another way

--- buildSrc/src/main/kotlin/GenerateGitHashTask.kt
+++ buildSrc/src/main/kotlin/GenerateGitHashTask.kt
@@ -10,16 +10,24 @@ abstract class GenerateGitHashTask : DefaultTask() {
 
     @TaskAction
     fun run() {
-        val head = project.layout.projectDirectory.file(".git/HEAD").asFile
+        val head = file("${project.rootDir}/.git/HEAD")
         val hash = if (head.exists()) {
             val text = head.readText().trim()
             if (text.startsWith("ref:")) {
                 val ref = text.removePrefix("ref:").trim()
-                val commitFile = project.layout.projectDirectory.file(".git/$ref").asFile
+                val commitFile = file("${project.rootDir}/.git/$ref")
                 if (commitFile.exists()) commitFile.readText().trim() else ""
             } else text
         } else ""
 
-        outputFile.get().asFile.writeText(hash.take(7))
+        val content = """
+            package com.lagradost.cloudstream3
+            object GitInfo {
+                const val HASH = "${hash.take(7)}"
+            }
+        """.trimIndent()
+
+        outputFile.get().asFile.parentFile.mkdirs()
+        outputFile.get().asFile.writeText(content)
     }
 }
