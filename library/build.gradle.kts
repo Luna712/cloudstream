f4d710fb38ed6402453d5e9506e9041387247e58
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-27 13:08:28 -0700

    Add

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -39,7 +39,8 @@ kotlin {
     compilerOptions {
         freeCompilerArgs.addAll(
             "-Xexpect-actual-classes",
-            "-Xannotation-default-target=param-property"
+            "-Xannotation-default-target=param-property",
+            "-Xreturn-value-checker=check"
         )
     }
 
