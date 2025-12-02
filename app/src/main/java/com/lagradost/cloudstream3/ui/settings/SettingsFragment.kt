2645f1309f8737762cbbdf2f658eaf7b2882d767
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-12-01 18:44:23 -0700

    Update

--- app/src/main/java/com/lagradost/cloudstream3/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/settings/SettingsFragment.kt
@@ -248,7 +248,7 @@ class SettingsFragment : BaseFragment<MainSettingsBinding>(
         }
 
         val appVersion = getString(R.string.app_version)
-        val commitHash = activity.currentCommitHash()
+        val commitHash = activity?.currentCommitHash() ?: ""
         val buildTimestamp = SimpleDateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG,
             Locale.getDefault()
         ).apply { timeZone = TimeZone.getTimeZone("UTC")
