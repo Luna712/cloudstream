e5c19b2c916f393a7f824dff531894e6c13ac266
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-12-05 15:02:36 -0700

    Remove

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -8,7 +8,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 plugins {
     id("maven-publish") // Gradle core plugin
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.lint)
     alias(libs.plugins.android.library)
     alias(libs.plugins.buildkonfig)
     alias(libs.plugins.dokka)
