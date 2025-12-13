785c0bc44e8e800448a5dc57360d39adfda07d26
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-12-13 13:50:10 -0700

    Update

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/AppDebug.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/AppDebug.kt
@@ -2,7 +2,7 @@ package com.lagradost.cloudstream3.utils
 
 import com.lagradost.cloudstream3.Prerelease
 
-@Prerelease
+@InternalAPI
 object AppDebug {
     @Volatile
     var isDebug: Boolean = false
