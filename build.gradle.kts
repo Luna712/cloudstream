a36818f70a46ddf6c6fcd1f9dbe853eabbcc9c45
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-12-21 14:28:48 -0700

    -

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,10 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
 }
+
+allprojects {
+    // https://docs.gradle.org/current/userguide/upgrading_major_version_9.html#test_task_fails_when_no_tests_are_discovered
+    tasks.withType<AbstractTestTask>().configureEach {
+        failOnNoDiscoveredTests = false
+    }
+}
