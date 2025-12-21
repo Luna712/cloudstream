3adff4e23231aeabf01ed6cd210c25092583d7b8
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-12-20 19:13:44 -0700

    -

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -7,7 +7,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     id("maven-publish") // Gradle core plugin
-    alias(libs.plugins.android.lint)
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.lint)
     alias(libs.plugins.android.multiplatform.library)
@@ -33,7 +32,6 @@ kotlin {
 
         lint {
             targetSdk = libs.versions.targetSdk.get().toInt()
-            abortOnError = false
         }
     }
 
