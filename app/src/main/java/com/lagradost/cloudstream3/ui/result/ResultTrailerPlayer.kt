bcec14343b72157b83c48f3b4c1fd915ac568a80
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-04-26 11:11:22 -0600

    Separate trailer

--- app/src/main/java/com/lagradost/cloudstream3/ui/result/ResultTrailerPlayer.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/result/ResultTrailerPlayer.kt
@@ -26,11 +26,13 @@ import com.lagradost.cloudstream3.utils.BackPressedCallbackHelper.detachBackPres
 import com.lagradost.cloudstream3.utils.UIHelper.fixSystemBarsPadding
 import com.lagradost.cloudstream3.utils.UIHelper.getStatusBarHeight
 
-class ResultTrailerPlayer : ResultFragmentPhone() {
+class ResultTrailerPlayer : BaseFragment<TrailerCustomLayoutBinding>(
+    BindingCreator.Inflate(TrailerCustomLayoutBinding::inflate)
+), PlayerView.Callbacks {
 
-    override var lockRotation = false
-    override var isFullScreenPlayer = false
-    override var hasPipModeSupport = false
+    private var lockRotation = false
+    private var isFullScreenPlayer = false
+    private var hasPipModeSupport = false
 
     companion object {
         const val TAG = "ResultTrailerPlayer"
@@ -38,6 +40,105 @@ class ResultTrailerPlayer : ResultFragmentPhone() {
 
     private var playerWidthHeight: Pair<Int, Int>? = null
     private var introVisible = true
+
+    private var player: IPlayer = CS3IPlayer()
+    private var hasPipModeSupport: Boolean = false
+    private var isFullScreenPlayer: Boolean = true
+    private var lockRotation: Boolean = true
+    private var isShowing: Boolean = false
+
+    private var playerHostView: PlayerView? = null
+
+    private fun uiReset() {
+        isShowing = false
+        updateUIVisibility()
+    }
+
+    override fun fixLayout(view: View) {
+        fixSystemBarsPadding(view)
+    }
+
+    var currentTrailers: List<Pair<ExtractorLink, String>> = emptyList()
+    var currentTrailerIndex = 0
+
+    override fun nextMirror() {
+        currentTrailerIndex++
+        loadTrailer()
+    }
+
+    override fun hasNextMirror(): Boolean {
+        return currentTrailerIndex + 1 < currentTrailers.size
+    }
+
+    override fun playerError(exception: Throwable) {
+        if (player.getIsPlaying()) { // because we don't want random toasts in player
+            playerHostView?.playerError(exception)
+        } else nextMirror()
+    }
+
+    private fun loadTrailer(index: Int? = null) {
+        val isSuccess =
+            currentTrailers.getOrNull(index ?: currentTrailerIndex)
+                ?.let { (extractedTrailerLink, _) ->
+                    context?.let { ctx ->
+                        player.onPause()
+                        player.loadPlayer(
+                            ctx,
+                            false,
+                            extractedTrailerLink,
+                            null,
+                            startPosition = 0L,
+                            subtitles = emptySet(),
+                            subtitle = null,
+                            autoPlay = false,
+                            preview = false
+                        )
+                        true
+                    } ?: run {
+                        false
+                    }
+                } ?: run {
+                false
+            }
+
+        val turnVis = !isSuccess && !isFullScreenPlayer
+        resultBinding?.apply {
+            // If we load a trailer, then cancel the big logo and only show the small title
+            if (isSuccess) {
+                // This is still a bit of a race condition, but it should work if we have the
+                // trailers observe after the page observe!
+                bindLogo(
+                    url = null,
+                    headers = null,
+                    logoView = backgroundPosterWatermarkBadge,
+                    titleView = resultTitle
+                )
+            }
+            resultSmallscreenHolder.isVisible = turnVis
+            resultPosterBackgroundHolder.apply {
+                val fadeIn: Animation = AlphaAnimation(alpha, if (turnVis) 1.0f else 0.0f).apply {
+                    interpolator = DecelerateInterpolator()
+                    duration = 200
+                    fillAfter = true
+                }
+                clearAnimation()
+                startAnimation(fadeIn)
+            }
+
+            // We don't want the trailer to be focusable if it's not visible
+            resultSmallscreenHolder.descendantFocusability = if (isSuccess) {
+                ViewGroup.FOCUS_AFTER_DESCENDANTS
+            } else ViewGroup.FOCUS_BLOCK_DESCENDANTS
+            oldbinding?.resultFullscreenHolder?.isVisible = !isSuccess && isFullScreenPlayer
+        }
+    }
+
+    internal fun setTrailers(trailers: List<Pair<ExtractorLink, String>>?) {
+        context?.updateHasTrailers()
+        if (!LoadResponse.isTrailersEnabled) return
+        currentTrailers = trailers?.sortedBy { -it.first.quality } ?: emptyList()
+        loadTrailer()
+    }
 
     // Single-tap on empty player area: toggle controls.
     override fun onSingleTap() {
@@ -55,7 +156,7 @@ class ResultTrailerPlayer : ResultFragmentPhone() {
 
     override fun isValidTouch(rawX: Float, rawY: Float): Boolean {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            val insets = playerBinding?.playerHolder?.rootWindowInsets
+            val insets = binding.playerHolder.rootWindowInsets
                 ?.getInsetsIgnoringVisibility(WindowInsets.Type.systemBars()) ?: return true
             return rawY > insets.top && rawX < (screenWidthWithOrientation - insets.right)
         }
@@ -82,7 +183,7 @@ class ResultTrailerPlayer : ResultFragmentPhone() {
     }
 
     private fun fixPlayerSize() {
-        binding?.apply {
+        oldbinding?.apply {
             if (isFullScreenPlayer) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
                     ViewCompat.setOnApplyWindowInsetsListener(root, null)
@@ -104,28 +205,28 @@ class ResultTrailerPlayer : ResultFragmentPhone() {
 
             val sw = if (orientation == Configuration.ORIENTATION_LANDSCAPE) screenWidth else screenHeight
 
-            resultBinding?.resultSmallscreenHolder?.isVisible = !isFullScreenPlayer
-            binding?.resultFullscreenHolder?.isVisible = isFullScreenPlayer
+            // resultBinding?.resultSmallscreenHolder?.isVisible = !isFullScreenPlayer
+            // oldbinding?.resultFullscreenHolder?.isVisible = isFullScreenPlayer
 
             val to = sw * h / w
 
-            resultBinding?.fragmentTrailer?.playerBackground?.apply {
+            /*resultBinding?.fragmentTrailer?.playerBackground?.apply {
                 isVisible = true
                 layoutParams = FrameLayout.LayoutParams(
                     FrameLayout.LayoutParams.MATCH_PARENT,
                     if (isFullScreenPlayer) FrameLayout.LayoutParams.MATCH_PARENT else to
                 )
-            }
+            }*/
 
-            playerBinding?.playerIntroPlay?.apply {
+            binding.playerIntroPlay.apply {
                 layoutParams = FrameLayout.LayoutParams(
                     FrameLayout.LayoutParams.MATCH_PARENT,
-                    resultBinding?.resultTopHolder?.measuredHeight ?: FrameLayout.LayoutParams.MATCH_PARENT
+                    /*resultBinding?.resultTopHolder?.measuredHeight ?:*/ FrameLayout.LayoutParams.MATCH_PARENT
                 )
             }
 
-            if (playerBinding?.playerIntroPlay?.isGone == true) {
-                resultBinding?.resultTopHolder?.apply {
+            if (binding.playerIntroPlay.isGone == true) {
+                /*resultBinding?.resultTopHolder?.apply {
                     val anim = ValueAnimator.ofInt(
                         measuredHeight,
                         if (isFullScreenPlayer) ViewGroup.LayoutParams.MATCH_PARENT else to
@@ -138,7 +239,7 @@ class ResultTrailerPlayer : ResultFragmentPhone() {
                     }
                     anim.duration = 200
                     anim.start()
-                }
+                }*/
             }
         }
     }
@@ -153,22 +254,13 @@ class ResultTrailerPlayer : ResultFragmentPhone() {
         }
     }
 
-    override fun showMirrorsDialogue() {}
-    override fun showTracksDialogue() {}
-
-    override fun openOnlineSubPicker(
-        context: Context,
-        loadResponse: LoadResponse?,
-        dismissCallback: () -> Unit
-    ) {}
-
     override fun subtitlesChanged() {}
     override fun embeddedSubtitlesFetched(subtitles: List<SubtitleData>) {}
     override fun onTracksInfoChanged() {}
     override fun exitedPipMode() {}
 
     override fun onSeekPreviewText(text: String?) {
-        playerBinding?.playerTimeText?.apply {
+        binding.playerTimeText.apply {
             isVisible = text != null
             if (text != null) this.text = text
         }
@@ -179,22 +271,22 @@ class ResultTrailerPlayer : ResultFragmentPhone() {
         lockRotation = fullscreen
         playerHostView?.isFullScreen = fullscreen
 
-        playerBinding?.playerFullscreen?.setImageResource(
+        binding.playerFullscreen.setImageResource(
             if (fullscreen) R.drawable.baseline_fullscreen_exit_24 else R.drawable.baseline_fullscreen_24
         )
         if (fullscreen) {
             playerHostView?.enterFullscreen()
-            binding?.apply {
+            /*oldbinding?.apply {
                 resultTopBar.isVisible = false
                 resultFullscreenHolder.isVisible = true
                 resultMainHolder.isVisible = false
             }
             resultBinding?.fragmentTrailer?.playerBackground?.let { view ->
                 (view.parent as ViewGroup?)?.removeView(view)
-                binding?.resultFullscreenHolder?.addView(view)
-            }
+                oldbinding?.resultFullscreenHolder?.addView(view)
+            }*/
         } else {
-            binding?.apply {
+            /*oldbinding?.apply {
                 resultTopBar.isVisible = true
                 resultFullscreenHolder.isVisible = false
                 resultMainHolder.isVisible = true
@@ -202,7 +294,7 @@ class ResultTrailerPlayer : ResultFragmentPhone() {
                     (view.parent as ViewGroup?)?.removeView(view)
                     resultBinding?.resultSmallscreenHolder?.addView(view)
                 }
-            }
+            }*/
             playerHostView?.exitFullscreen()
         }
         fixPlayerSize()
@@ -215,9 +307,8 @@ class ResultTrailerPlayer : ResultFragmentPhone() {
         }
     }
 
-    override fun updateUIVisibility() {
-        super.updateUIVisibility()
-        playerBinding?.apply {
+    private fun updateUIVisibility() {
+        binding.apply {
             playerGoBackHolder.isVisible = false
             val controlsVisible = isShowing && !introVisible
             playerTopHolder.isVisible = controlsVisible
@@ -232,26 +323,69 @@ class ResultTrailerPlayer : ResultFragmentPhone() {
 
     override fun playerStatusChanged() {
         if (introVisible) {
-            playerBinding?.playerPausePlayHolderHolder?.isVisible = false
+            binding.playerPausePlayHolderHolder.isVisible = false
         }
     }
 
-    override fun onBindingCreated(binding: FragmentResultSwipeBinding, savedInstanceState: Bundle?) {
-        super.onBindingCreated(binding, savedInstanceState)
+    override fun onBindingCreated(binding: TrailerCustomLayoutBinding) {
+        /*FragmentResultSwipeBinding.bind(root).let { bind ->
+            resultBinding = bind.fragmentResult
+            recommendationBinding = bind.resultRecommendations
+            syncBinding = bind.resultSync
+            binding = bind
+        }*/
+        val ctx = context ?: return
+        playerHostView = PlayerView(ctx)
+        playerHostView?.player = player
+        playerHostView?.hasPipModeSupport = hasPipModeSupport
+        playerHostView?.callbacks = this
+        playerHostView?.bindViews(binding.root)
+        playerHostView?.initialize()
 
-        playerHostView?.videoOutline = playerBinding?.videoOutline
+        playerHostView?.videoOutline = binding.videoOutline
         playerHostView?.requestUpdateBrightnessOverlayOnNextLayout()
 
-        playerBinding?.playerFullscreen?.setOnClickListener { updateFullscreen(!isFullScreenPlayer) }
+        binding.playerFullscreen.setOnClickListener { updateFullscreen(!isFullScreenPlayer) }
         updateFullscreen(isFullScreenPlayer)
         uiReset()
 
-        playerBinding?.playerIntroPlay?.setOnClickListener {
-            playerBinding?.playerIntroPlay?.isGone = true
+        binding.playerIntroPlay.setOnClickListener {
+            binding.playerIntroPlay.isGone = true
             introVisible = false
             player.handleEvent(CSPlayerEvent.Play, PlayerEventSource.UI)
             fixPlayerSize()
             showControls()
         }
+
+        binding.apply {
+            playerOpenSource.setOnClickListener {
+                currentTrailers.getOrNull(currentTrailerIndex)?.let { (_, ogTrailerLink) ->
+                    context?.openBrowser(ogTrailerLink)
+                }
+            }
+        }
+    }
+
+    override fun onPause() {
+        playerHostView?.releaseKeyEventListener()
+        super.onPause()
+    }
+
+    override fun onResume() {
+        context?.let { ctx ->
+            playerHostView?.onResume(ctx)
+            playerHostView?.setupKeyEventListener()
+        }
+        super.onResume()
+    }
+
+    override fun onStop() {
+        playerHostView?.onStop()
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        playerHostView?.release()
+        super.onDestroyView()
     }
 }
