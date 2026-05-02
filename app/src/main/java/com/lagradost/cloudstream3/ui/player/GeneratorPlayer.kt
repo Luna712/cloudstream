6d250262d026527ff7de3a71482f21b5fa3d8b60
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-02 11:10:03 -0600

    Test player fix

--- app/src/main/java/com/lagradost/cloudstream3/ui/player/GeneratorPlayer.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/player/GeneratorPlayer.kt
@@ -162,8 +162,10 @@ class GeneratorPlayer : FullScreenPlayer() {
 
     private lateinit var viewModel: PlayerGeneratorViewModel //by activityViewModels()
     private lateinit var sync: SyncViewModel
-    private var currentLinks: Set<Pair<ExtractorLink?, ExtractorUri?>> = setOf()
-    private var currentSubs: Set<SubtitleData> = setOf()
+    private var currentLinks: Set<Pair<ExtractorLink?, ExtractorUri?>>
+        get() = viewModel.currentLinks ?: emptySet()
+    private var currentSubs: Set<SubtitleData>
+        get() = viewModel.currentSubs ?: emptySet()
 
     private var currentSelectedLink: Pair<ExtractorLink?, ExtractorUri?>? = null
     private var currentSelectedSubtitles: SubtitleData? = null
