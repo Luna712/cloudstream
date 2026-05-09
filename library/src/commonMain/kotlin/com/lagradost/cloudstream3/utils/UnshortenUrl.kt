17121eab4f4fba56c03d00753fad0ebb5c199911
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-08 19:54:08 -0600

    Update

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/UnshortenUrl.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/UnshortenUrl.kt
@@ -3,7 +3,7 @@ package com.lagradost.cloudstream3.utils
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.base64Decode
 import com.lagradost.cloudstream3.ksoupDocument
-import com.lagradost.nicehttp.kmp.NiceResponse
+import com.lagradost.nicehttp.kmp.INiceResponse
 import java.net.URI
 import java.net.URLDecoder
 
@@ -108,7 +108,7 @@ object ShortLink {
     }
 
     suspend fun unshortenLinkup(uri: String): String {
-        var r: NiceResponse? = null
+        var r: INiceResponse? = null
         var uri = uri
         when {
             uri.contains("/tv/") -> uri = uri.replace("/tv/", "/tva/")
