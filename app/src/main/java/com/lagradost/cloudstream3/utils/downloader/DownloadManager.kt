018e210cfbf9f03f8e7cdbe5b49571be56a7c097
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-09 21:16:18 -0600

    Fix

--- app/src/main/java/com/lagradost/cloudstream3/utils/downloader/DownloadManager.kt
+++ app/src/main/java/com/lagradost/cloudstream3/utils/downloader/DownloadManager.kt
@@ -820,7 +820,7 @@ object VideoDownloadManager {
                 referer = referer,
                 verify = false
             )
-            val requestStream = response.body.bytes().inputStream()
+            val requestStream = request.body.bytes().inputStream()
 
             val buffer = ByteArray(bufferSize)
             var read: Int
