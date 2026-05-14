21d200a52c8c20bf665d4207de17c94941a508e0
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-14 12:45:25 -0600

    Fix

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/MainActivity.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/MainActivity.kt
@@ -9,6 +9,7 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.serializer
+import kotlinx.serialization.serializerOrNull
 import kotlin.reflect.KClass
 
 // Short name for requests client to make it nicer to use
