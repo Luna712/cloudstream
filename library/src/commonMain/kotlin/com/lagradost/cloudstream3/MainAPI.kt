d7112aca304f6c81506ca2bdc9a9f12a68985a7d
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-14 16:07:39 -0600

    name

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/MainAPI.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/MainAPI.kt
@@ -76,7 +76,7 @@ class ErrorLoadingException(message: String? = null) : Exception(message)
 
 //val baseHeader = mapOf("User-Agent" to USER_AGENT)
 
-val json = Json { ignoreUnknownKeys = true }
+val appJson = Json { ignoreUnknownKeys = true }
 val mapper = JsonMapper.builder().addModule(kotlinModule())
     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).build()!!
 
