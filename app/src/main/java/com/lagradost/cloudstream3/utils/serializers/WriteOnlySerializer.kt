d37c3af413ee392218726b659715b36a8ee10cd0
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-15 21:52:02 -0600

    Add

---
+++ app/src/main/java/com/lagradost/cloudstream3/utils/serializers/WriteOnlySerializer.kt
@@ -0,0 +1,36 @@
+package com.lagradost.cloudstream3.utils.serializers
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonTransformingSerializer
+
+/**
+ * Replicates Jackson's @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) behaviour.
+ * Properties in [keysToIgnore] are deserialized normally but omitted from serialized output.
+ *
+ * Cannot be used directly with @Serializable(with = ...) since annotations don't support
+ * constructor arguments. Instead, create a named object in the target class that extends
+ * this and passes the keys to ignore:
+ *
+ *   @Serializable(with = MyData.Serializer::class)
+ *   data class MyData(
+ *       val fieldA: String,
+ *       val fieldB: String
+ *   ) {
+ *       object Serializer : WriteOnlySerializer<MyData>(
+ *           MyData.serializer(),
+ *           setOf("fieldB")
+ *       )
+ *   }
+ */
+abstract class WriteOnlySerializer<T : Any>(
+    tSerializer: KSerializer<T>,
+    private val keysToIgnore: Set<String>
+) : JsonTransformingSerializer<T>(tSerializer) {
+
+    override fun transformSerialize(element: JsonElement): JsonElement {
+        if (element !is JsonObject) return element
+        return JsonObject(element.filterKeys { it !in keysToIgnore })
+    }
+}
