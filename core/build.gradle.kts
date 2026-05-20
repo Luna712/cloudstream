5514c60d00dba3ea07dceab2ba6e4785162cec3d
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-20 14:08:29 -0600

    Add

---
+++ core/build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.multiplatform.library)
+}
+
+val javaTarget = JvmTarget.fromTarget(libs.versions.jvmTarget.get())
+
+kotlin {
+    android {
+        namespace = "com.lagradost.cloudstream3.core"
+        compileSdk = libs.versions.compileSdk.get().toInt()
+        minSdk = libs.versions.minSdk.get().toInt()
+
+        compilerOptions {
+            jvmTarget.set(javaTarget)
+        }
+    }
+
+    jvm()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.coroutines.core)
+        }
+
+        androidMain.dependencies {
+            implementation(libs.preference.ktx)
+        }
+    }
+}
+
+tasks.withType<KotlinJvmCompile> {
+    compilerOptions {
+        jvmTarget.set(javaTarget)
+    }
+}
