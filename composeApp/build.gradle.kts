02219dcdcb9b87d0bc92168d8da4c8a9928d7998
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-20 18:41:49 -0600

    Add

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -32,6 +32,7 @@ kotlin {
             implementation(libs.compose.runtime)
             implementation(libs.compose.foundation)
             implementation(libs.compose.material3)
+            implementation(compose.materialIconsExtended)
             implementation(libs.compose.ui)
             implementation(libs.compose.resources)
             implementation(project(":core"))
