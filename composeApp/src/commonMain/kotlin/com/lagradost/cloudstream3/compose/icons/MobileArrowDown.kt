3c438376f328a4a77c146af1c9007981aee61ccc
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-20 19:56:42 -0600

    Fix

--- composeApp/src/commonMain/kotlin/com/lagradost/cloudstream3/compose/icons/MobileArrowDown.kt
+++ composeApp/src/commonMain/kotlin/com/lagradost/cloudstream3/compose/icons/MobileArrowDown.kt
@@ -10,14 +10,14 @@ import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
 
 @Suppress("CheckReturnValue")
-public val storage: ImageVector
+public val mobile_arrow_down: ImageVector
   get() {
-    if (_storage != null) {
-      return _storage!!
+    if (_mobile_arrow_down != null) {
+      return _mobile_arrow_down!!
     }
-    _storage =
+    _mobile_arrow_down =
       ImageVector.Builder(
-          name = "storage",
+          name = "mobile_arrow_down",
           defaultWidth = 24.dp,
           defaultHeight = 24.dp,
           viewportWidth = 24f,
@@ -35,46 +35,51 @@ public val storage: ImageVector
             strokeLineMiter = 1f,
             pathFillType = PathFillType.Companion.NonZero,
           ) {
-            moveTo(3f, 20f)
-            verticalLineTo(16f)
-            horizontalLineTo(21f)
-            verticalLineToRelative(4f)
-            horizontalLineTo(3f)
-            close()
-            moveTo(5f, 19f)
-            horizontalLineTo(7f)
-            verticalLineTo(17f)
-            horizontalLineTo(5f)
+            moveTo(7f, 23f)
+            quadTo(6.18f, 23f, 5.59f, 22.41f)
+            reflectiveQuadTo(5f, 21f)
+            verticalLineTo(3f)
+            quadTo(5f, 2.17f, 5.59f, 1.59f)
+            reflectiveQuadTo(7f, 1f)
+            horizontalLineTo(17f)
+            quadToRelative(0.82f, 0f, 1.41f, 0.59f)
+            reflectiveQuadTo(19f, 3f)
+            verticalLineTo(6.1f)
+            quadToRelative(0.45f, 0.18f, 0.73f, 0.55f)
+            reflectiveQuadTo(20f, 7.5f)
             verticalLineToRelative(2f)
+            quadToRelative(0f, 0.47f, -0.27f, 0.85f)
+            reflectiveQuadTo(19f, 10.9f)
+            verticalLineTo(21f)
+            quadToRelative(0f, 0.82f, -0.59f, 1.41f)
+            reflectiveQuadTo(17f, 23f)
+            horizontalLineTo(7f)
             close()
-            moveTo(3f, 8f)
-            verticalLineTo(4f)
-            horizontalLineTo(21f)
+            moveTo(7f, 21f)
+            horizontalLineTo(17f)
+            verticalLineTo(3f)
+            horizontalLineTo(7f)
+            verticalLineTo(21f)
+            close()
+            moveToRelative(0f, 0f)
+            verticalLineTo(3f)
+            verticalLineTo(21f)
+            close()
+            moveToRelative(5f, -5f)
+            lineToRelative(4f, -4f)
+            lineTo(14.6f, 10.6f)
+            lineTo(13f, 12.15f)
             verticalLineTo(8f)
-            horizontalLineTo(3f)
-            close()
-            moveTo(5f, 7f)
-            horizontalLineTo(7f)
-            verticalLineTo(5f)
-            horizontalLineTo(5f)
-            verticalLineTo(7f)
-            close()
-            moveTo(3f, 14f)
-            verticalLineTo(10f)
-            horizontalLineTo(21f)
-            verticalLineToRelative(4f)
-            horizontalLineTo(3f)
-            close()
-            moveTo(5f, 13f)
-            horizontalLineTo(7f)
-            verticalLineTo(11f)
-            horizontalLineTo(5f)
-            verticalLineToRelative(2f)
+            horizontalLineTo(11f)
+            verticalLineToRelative(4.15f)
+            lineTo(9.4f, 10.6f)
+            lineTo(8f, 12f)
+            lineToRelative(4f, 4f)
             close()
           }
         }
         .build()
-    return _storage!!
+    return _mobile_arrow_down!!
   }
 
-private var _storage: ImageVector? = null
+private var _mobile_arrow_down: ImageVector? = null
