67451279ead3a69291c81481204ba26d12f316a8
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-21 17:24:20 -0600

    Fix

--- sharedLogic/src/androidMain/kotlin/com/lagradost/cloudstream3/shared/DeviceInfo.android.kt
+++ sharedLogic/src/androidMain/kotlin/com/lagradost/cloudstream3/shared/DeviceInfo.android.kt
@@ -5,7 +5,7 @@ import android.content.Context
 import android.content.res.Configuration
 import android.os.Build
 import androidx.preference.PreferenceManager
-import com.lagradost.api.ContextHelper
+import com.lagradost.api.getContext
 import com.lagradost.cloudstream3.shared.preferences.PreferenceKeys
 
 internal actual object DeviceInfo {
@@ -14,13 +14,13 @@ internal actual object DeviceInfo {
     actual fun getModel(): String = Build.MODEL
 
     actual fun getUIMode(): Int {
-        val context = ContextHelper.getContext() ?: return 0
+        val context = getContext() ?: return 0
         val uiModeManager = context.getSystemService(Context.UI_MODE_SERVICE) as UiModeManager?
         return uiModeManager?.currentModeType ?: 0
     }
 
     actual fun getLayoutPreference(): Int {
-        val context = ContextHelper.getContext() ?: return -1
+        val context = getContext() ?: return -1
         return PreferenceManager.getDefaultSharedPreferences(context)
             .getInt(PreferenceKeys.APP_LAYOUT, -1)
     }
