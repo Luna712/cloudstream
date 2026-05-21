cc43718da251f19ba0fc4f55f052a4e3b30d2f16
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-21 17:17:14 -0600

    Add

--- sharedLogic/build.gradle.kts
+++ sharedLogic/build.gradle.kts
@@ -24,6 +24,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(libs.kotlinx.coroutines.core)
+            implementation(project(":library"))
         }
 
         androidMain.dependencies {
