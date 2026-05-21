12fb2be37c56a037f102caa20ee506448ce1e511
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-20 19:06:53 -0600

    Remove imports

--- composeApp/src/commonMain/kotlin/com/lagradost/cloudstream3/compose/settings/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/lagradost/cloudstream3/compose/settings/SettingsScreen.kt
@@ -9,15 +9,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.outlined.ArrowForwardIos
-import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material.icons.outlined.Extension
-import androidx.compose.material.icons.outlined.Palette
-import androidx.compose.material.icons.outlined.PlayCircle
-import androidx.compose.material.icons.outlined.Storage
-import androidx.compose.material.icons.outlined.SystemUpdate
-import androidx.compose.material.icons.outlined.Tune
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
