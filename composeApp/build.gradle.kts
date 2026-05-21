5875c4a276e41c7af6e31e20da6ebe36b8e86b34
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-20 19:03:36 -0600

    Remove

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -32,7 +32,6 @@ kotlin {
             implementation(libs.compose.runtime)
             implementation(libs.compose.foundation)
             implementation(libs.compose.material3)
-            implementation(compose.materialIconsExtended)
             implementation(libs.compose.ui)
             implementation(libs.compose.resources)
             implementation(project(":core"))
