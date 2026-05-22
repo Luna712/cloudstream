7006c973fe6a0e43627e8249ac578e0e6ddce7d0
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-22 15:04:01 -0600

    const

--- sharedLogic/build.gradle.kts
+++ sharedLogic/build.gradle.kts
@@ -67,7 +67,7 @@ buildkonfig {
     exposeObjectWithName = "BuildConfig"
     
     defaultConfigs {
-        buildConfigField(FieldSpec.Type.STRING, "GIT_HASH", gitHashProvider.get())
-        buildConfigField(FieldSpec.Type.LONG, "BUILD_DATE", buildDateProvider.get().toString())
+        buildConfigField(FieldSpec.Type.STRING, "GIT_HASH", gitHashProvider.get(), const = true)
+        buildConfigField(FieldSpec.Type.LONG, "BUILD_DATE", buildDateProvider.get().toString(), const = true)
     }
 }
