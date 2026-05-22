1575a07939d779e16d95f3798ebce5d5b21774ab
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-21 21:42:57 -0600

    Change

--- sharedLogic/src/commonMain/kotlin/com/lagradost/cloudstream3/shared/DeviceInfo.kt
+++ sharedLogic/src/commonMain/kotlin/com/lagradost/cloudstream3/shared/DeviceInfo.kt
@@ -1,8 +1,7 @@
 package com.lagradost.cloudstream3.shared
 
 internal expect object DeviceInfo {
-    val UI_MODE_TELEVISION: Int
+    fun isUIModeTV(): Boolean
     fun getModel(): String
-    fun getUIMode(): Int
     fun getLayoutPreference(): Int
 }
