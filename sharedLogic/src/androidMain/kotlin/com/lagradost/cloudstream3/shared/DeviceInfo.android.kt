6ba4f70a98aa772b1ffbd13ce8c2e98981ee16dc
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-21 21:42:21 -0600

    Change

--- sharedLogic/src/androidMain/kotlin/com/lagradost/cloudstream3/shared/DeviceInfo.android.kt
+++ sharedLogic/src/androidMain/kotlin/com/lagradost/cloudstream3/shared/DeviceInfo.android.kt
@@ -9,15 +9,13 @@ import com.lagradost.api.getContext
 import com.lagradost.cloudstream3.shared.preferences.PreferenceKeys
 
 internal actual object DeviceInfo {
-    actual const val UI_MODE_TELEVISION: Int = Configuration.UI_MODE_TYPE_TELEVISION
+    actual fun isUIModeTV(): Boolean {
+        val context = getContext() as? Context ?: return false
+        val uiModeManager = context.getSystemService(Context.UI_MODE_SERVICE) as UiModeManager?
+        return uiModeManager?.currentModeType == Configuration.UI_MODE_TYPE_TELEVISION
+    }
 
     actual fun getModel(): String = Build.MODEL
-
-    actual fun getUIMode(): Int {
-        val context = getContext() as? Context ?: return 0
-        val uiModeManager = context.getSystemService(Context.UI_MODE_SERVICE) as UiModeManager?
-        return uiModeManager?.currentModeType ?: 0
-    }
 
     actual fun getLayoutPreference(): Int {
         val context = getContext() as? Context ?: return -1
