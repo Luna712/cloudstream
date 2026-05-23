eecea0a87281f83a7ac589d79886b0b31804fd0d
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-22 18:27:29 -0600

    -

--- composeApp/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/DeviceInfo.kt
+++ composeApp/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/DeviceInfo.kt
@@ -1,7 +1,7 @@
 package com.lagradost.cloudstream3
 
 internal actual object DeviceInfo {
-    actual fun isTVDevice(): Boolean = false
-    actual fun isLandscape(): Boolean = false
-    actual fun getLayoutPreference(): Int = -1
+    actual fun isTVDevice(): Boolean
+    actual fun isLandscape(): Boolean
+    actual fun getLayoutPreference(): Int
 }
