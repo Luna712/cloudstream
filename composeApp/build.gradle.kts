0001272739f4c0b1e71468b0a6f43db8caae1efc
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-22 20:06:49 -0600

    Fix jvm

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -27,7 +27,15 @@ kotlin {
         }
     }
 
-    jvm()
+    jvm {
+        compilations.all {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    jvmTarget.set(javaTarget)
+                }
+            }
+        }
+    }
 
     compilerOptions {
         freeCompilerArgs.add("-Xexpect-actual-classes")
