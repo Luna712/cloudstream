cecc4b9da51af902e2cdda15e36af28389c893e5
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-22 21:43:34 -0600

    Add DeviceType

--- composeApp/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/DeviceInfo.kt
+++ composeApp/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/DeviceInfo.kt
@@ -1,7 +1,11 @@
 package com.lagradost.cloudstream3
 
 internal expect object DeviceInfo {
-    fun isTVDevice(): Boolean
+    fun getDeviceType(): DeviceType
     fun isLandscape(): Boolean
     fun getLayoutPreference(): Int
+}
+
+enum class DeviceType {
+    PHONE, TV, EMULATOR, COMPUTER
 }
