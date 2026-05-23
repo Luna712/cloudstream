ce8ca5c8ccbc9475236edfac9f04a5f645759655
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-22 18:23:41 -0600

    Add isLandscape

--- composeApp/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/DeviceLayout.kt
+++ composeApp/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/DeviceLayout.kt
@@ -1,12 +1,15 @@
 package com.lagradost.cloudstream3
 
 object DeviceLayout {
-    const val PHONE    : Int = 0b001
-    const val TV       : Int = 0b010
-    const val EMULATOR : Int = 0b100
+    const val PHONE: Int = 0b001
+    const val TV: Int = 0b010
+    const val EMULATOR: Int = 0b100
 
     private val layoutId: Int get() = resolveLayout()
     fun isLayout(flags: Int): Boolean = (layoutId and flags) != 0
+
+    fun isLandscape(): Boolean =
+        isLayout(TV or EMULATOR) || DeviceInfo.isLandscape()
 
     private fun resolveLayout(): Int {
         return when (DeviceInfo.getLayoutPreference()) {
