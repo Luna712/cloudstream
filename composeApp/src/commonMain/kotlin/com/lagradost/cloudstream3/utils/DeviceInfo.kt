7ea6384603b70e435803a84e59b4516713adfed8
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-22 18:21:30 -0600

    Add isLandscape

--- composeApp/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/DeviceInfo.kt
+++ composeApp/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/DeviceInfo.kt
@@ -1,6 +1,7 @@
 package com.lagradost.cloudstream3
 
-internal expect object DeviceInfo {
-    fun isTVDevice(): Boolean
-    fun getLayoutPreference(): Int
+internal actual object DeviceInfo {
+    actual fun isTVDevice(): Boolean = false
+    actual fun isLandscape(): Boolean = false
+    actual fun getLayoutPreference(): Int = -1
 }
