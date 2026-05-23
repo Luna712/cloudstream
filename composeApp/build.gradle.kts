8c443ff9a3947920ef0137203a17044bbbf7ece4
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-22 19:45:37 -0600

    lint

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.lint)
     alias(libs.plugins.android.multiplatform.library)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
