36eaad34d744eb1162bc12791476044929cbfd92
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-24 21:07:20 -0600

    -

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/StreamTape.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/StreamTape.kt
@@ -34,7 +34,7 @@ open class StreamTape : ExtractorApi() {
                 this.document.select("script").firstOrNull { it.html().contains("botlink').innerHTML") }
                     ?.html()?.lines()?.firstOrNull { it.contains("botlink').innerHTML") }?.let {
                         val scriptContent = it.substringAfter(").innerHTML").replaceFirst("=", "var url =")
-                        evalJs(scriptContent, "url").getOrNull()?.toString()
+                        evalJs(scriptContent, "url")?.toString()
                     }
 
             if (!result.isNullOrEmpty()) {
