1b14d38fa029e7354447e1f5e7283f292827d4ae
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-26 13:18:59 -0600

    Try

--- composeApp/src/commonMain/kotlin/com/lagradost/cloudstream4/utils/DeviceInfo.kt
+++ composeApp/src/commonMain/kotlin/com/lagradost/cloudstream4/utils/DeviceInfo.kt
@@ -1,17 +1,10 @@
 package com.lagradost.cloudstream4.utils
 
-// DeviceType is intentionally hidden from IDE autocomplete and external callers.
-// Use DeviceLayout.isLayout() with DeviceLayout.PHONE, DeviceLayout.TV etc. instead.
-@Deprecated(
-    message = "Use DeviceLayout.isLayout() instead",
-    level = DeprecationLevel.HIDDEN,
-)
-internal enum class DeviceType {
-    PHONE, TV, EMULATOR, COMPUTER
-}
-
 internal expect object DeviceInfo {
-    @Suppress("DEPRECATION", "DEPRECATION_ERROR")
+    private enum class DeviceType {
+        PHONE, TV, EMULATOR, COMPUTER
+    }
+
     fun getDeviceType(): DeviceType
     fun isLandscape(): Boolean
     fun getLayoutPreference(): Int
