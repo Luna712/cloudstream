b95ac99ebd1e1a51bec30eaac77ce79bfe7916ae
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-26 16:39:01 -0600

    Update

--- composeApp/src/commonMain/kotlin/com/lagradost/cloudstream4/utils/DeviceInfo.kt
+++ composeApp/src/commonMain/kotlin/com/lagradost/cloudstream4/utils/DeviceInfo.kt
@@ -1,7 +1,14 @@
 package com.lagradost.cloudstream4.utils
 
+import kotlin.jvm.JvmInline
+
 internal expect object DeviceInfo {
-    fun getDeviceLayout(): DeviceLayout.Layout
+    fun getDeviceType(): DeviceType
     fun isLandscape(): Boolean
     fun getLayoutPreference(): Int
+}
+
+@JvmInline // This still works but has no affect on non-JVM targets
+value class DeviceType(val value: Int) {
+    infix fun or(other: DeviceType) = DeviceType(value or other.value)
 }
