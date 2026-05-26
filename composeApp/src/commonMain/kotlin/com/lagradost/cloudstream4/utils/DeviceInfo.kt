9f05a17872d20e696a8c29617f1aca42da7b77c1
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-26 13:41:00 -0600

    Update

--- composeApp/src/commonMain/kotlin/com/lagradost/cloudstream4/utils/DeviceInfo.kt
+++ composeApp/src/commonMain/kotlin/com/lagradost/cloudstream4/utils/DeviceInfo.kt
@@ -1,7 +1,7 @@
 package com.lagradost.cloudstream4.utils
 
 internal expect object DeviceInfo {
-    fun getDeviceLayout(): Int
+    fun getDeviceLayout(): DeviceLayout.Layout
     fun isLandscape(): Boolean
     fun getLayoutPreference(): Int
 }
