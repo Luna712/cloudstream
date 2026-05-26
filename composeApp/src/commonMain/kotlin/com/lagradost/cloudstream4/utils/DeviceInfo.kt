3509c081e02cc3937f6627d8ac5c0f789dfd9c51
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-26 13:20:54 -0600

    Rm

--- composeApp/src/commonMain/kotlin/com/lagradost/cloudstream4/utils/DeviceInfo.kt
+++ composeApp/src/commonMain/kotlin/com/lagradost/cloudstream4/utils/DeviceInfo.kt
@@ -1,11 +1,7 @@
 package com.lagradost.cloudstream4.utils
 
 internal expect object DeviceInfo {
-    private enum class DeviceType {
-        PHONE, TV, EMULATOR, COMPUTER
-    }
-
-    fun getDeviceType(): DeviceType
+    fun getDeviceType(): Int
     fun isLandscape(): Boolean
     fun getLayoutPreference(): Int
 }
