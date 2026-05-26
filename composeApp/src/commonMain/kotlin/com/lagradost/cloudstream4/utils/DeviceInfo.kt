edf8249d0fb75f4223ad46a615cec21fa6f17f31
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-26 13:00:17 -0600

    Update

--- composeApp/src/commonMain/kotlin/com/lagradost/cloudstream4/utils/DeviceInfo.kt
+++ composeApp/src/commonMain/kotlin/com/lagradost/cloudstream4/utils/DeviceInfo.kt
@@ -1,16 +1,17 @@
 package com.lagradost.cloudstream4.utils
 
-internal expect object DeviceInfo {
-    fun getDeviceType(): DeviceType
-    fun isLandscape(): Boolean
-    fun getLayoutPreference(): Int
-}
-
-// HIDDEN prevents this from showing up in auto completion at all.
+// DeviceType is intentionally hidden from IDE autocomplete and external callers.
+// Use DeviceLayout.isLayout() with DeviceLayout.PHONE, DeviceLayout.TV etc. instead.
 @Deprecated(
     message = "Use DeviceLayout.isLayout() instead",
     level = DeprecationLevel.HIDDEN,
 )
 internal enum class DeviceType {
     PHONE, TV, EMULATOR, COMPUTER
+}
+
+internal expect object DeviceInfo {
+    fun getDeviceType(): DeviceType
+    fun isLandscape(): Boolean
+    fun getLayoutPreference(): Int
 }
