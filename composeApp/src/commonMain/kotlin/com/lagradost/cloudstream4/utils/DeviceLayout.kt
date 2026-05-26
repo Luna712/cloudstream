2f19b3f9613aed805797c7257ffebfbd21a4b1b1
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-05-26 16:44:58 -0600

    Update

--- composeApp/src/commonMain/kotlin/com/lagradost/cloudstream4/utils/DeviceLayout.kt
+++ composeApp/src/commonMain/kotlin/com/lagradost/cloudstream4/utils/DeviceLayout.kt
@@ -1,14 +1,20 @@
 package com.lagradost.cloudstream4.utils
 
 import com.lagradost.cloudstream4.preferences.PreferenceDefaults
+import kotlin.jvm.JvmInline
 
 object DeviceLayout {
-    val PHONE = DeviceType(0b00001)
-    val TV = DeviceType(0b00010)
-    val EMULATOR = DeviceType(0b00100)
-    val COMPUTER = DeviceType(0b01000)
+    @JvmInline // This still works but has no affect on non-JVM targets
+    value class Layout(val value: Int) {
+        infix fun or(other: Layout) = Layout(value or other.value)
+    }
 
-    private var layoutId = DeviceType(PreferenceDefaults.APP_LAYOUT)
+    val PHONE = Layout(0b00001)
+    val TV = Layout(0b00010)
+    val EMULATOR = Layout(0b00100)
+    val COMPUTER = Layout(0b01000)
+
+    private var layoutId = Layout(PreferenceDefaults.APP_LAYOUT)
     // TODO when fully on Compose
     // private val layoutId: Int get() = resolveLayout()
 
@@ -38,9 +44,9 @@ object DeviceLayout {
         layoutId = resolveLayout()
     }
 
-    private fun resolveLayout(): DeviceType {
+    private fun resolveLayout(): Layout {
         return when (DeviceInfo.getLayoutPreference()) {
-            PreferenceDefaults.APP_LAYOUT -> DeviceInfo.getDeviceType()
+            PreferenceDefaults.APP_LAYOUT -> DeviceInfo.getDetectedLayout()
             0 -> PHONE
             1 -> TV
             2 -> EMULATOR
