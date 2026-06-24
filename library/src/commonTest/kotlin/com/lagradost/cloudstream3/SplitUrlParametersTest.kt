60b81f718c1a4dad6b89a3526ae7f6ede9c11b9b
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-06-23 20:20:35 -0600

    Update

--- library/src/commonTest/kotlin/com/lagradost/cloudstream3/SplitUrlParametersTest.kt
+++ library/src/commonTest/kotlin/com/lagradost/cloudstream3/SplitUrlParametersTest.kt
@@ -5,7 +5,7 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class splitUrlParametersTest {
+class SplitUrlParametersTest {
 
     @Test
     fun parsesBasicQueryParameters() {
