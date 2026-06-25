a70f75c5412641b28e0dd6226bcabd6c1eaf7e1c
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-06-25 15:08:01 -0600

    Update wcohelper

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/helper/WcoHelper.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/helper/WcoHelper.kt
@@ -1,8 +1,9 @@
 package com.lagradost.cloudstream3.extractors.helper
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.lagradost.cloudstream3.app
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import com.lagradost.cloudstream3.app
 
 class WcoHelper {
     companion object {
@@ -10,20 +11,15 @@ class WcoHelper {
 
         @Serializable
         data class ExternalKeys(
-            @SerialName("wco_key")
-            val wcoKey: String? = null,
-            @SerialName("wco_cipher_key")
-            val wcocipher: String? = null
+            @JsonProperty("wco_key") @SerialName("wco_key") val wcoKey: String? = null,
+            @JsonProperty("wco_cipher_key") @SerialName("wco_cipher_key") val wcocipher: String? = null,
         )
 
         @Serializable
         data class NewExternalKeys(
-            @SerialName("cipherKey")
-            val cipherkey: String? = null,
-            @SerialName("encryptKey")
-            val encryptKey: String? = null,
-            @SerialName("mainKey")
-            val mainKey: String? = null,
+            @JsonProperty("cipherKey") @SerialName("cipherKey") val cipherkey: String? = null,
+            @JsonProperty("encryptKey") @SerialName("encryptKey") val encryptKey: String? = null,
+            @JsonProperty("mainKey") @SerialName("mainKey") val mainKey: String? = null,
         )
 
         private var keys: ExternalKeys? = null
