bcc53e76c1bdcd2435a9c7d2f08c8dc8f9682772
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-06-29 13:19:50 -0600

    Update

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/serializers/WriteOnlySerializer.kt
+++ library/src/commonMain/kotlin/com/lagradost/cloudstream3/utils/serializers/WriteOnlySerializer.kt
@@ -16,8 +16,8 @@ import kotlinx.serialization.json.JsonTransformingSerializer
  *   @KeepGeneratedSerializer
  *   @Serializable(with = MyData.Serializer::class)
  *   data class MyData(
- *       val fieldA: String = "",
- *       val fieldB: String = "",
+ *       @SerialName("fieldA") val fieldA: String = "",
+ *       @SerialName("fieldB") val fieldB: String = "",
  *   ) {
  *       object Serializer : WriteOnlySerializer<MyData>(
  *           MyData.generatedSerializer(),
