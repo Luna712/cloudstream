c1a5c37575573e44aa48c4369c37da46c6631212
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-01 10:35:33 -0600

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -226,7 +226,7 @@ dependencies {
     // Android Core & Lifecycle
     implementation(libs.core.ktx)
     implementation(libs.activity.ktx)
-    implementation(libs.annotation)
+    implementation(libs.androidx.annotation)
     implementation(libs.appcompat)
     implementation(libs.fragment.ktx)
     implementation(libs.bundles.lifecycle)
