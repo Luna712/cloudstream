59bb2111f55b6b5ec3c3975031d0062a4a115064
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-03 10:10:45 -0600

    -

--- app/src/main/java/com/lagradost/cloudstream3/ui/APIRepository.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/APIRepository.kt
@@ -84,7 +84,6 @@ class APIRepository(val api: MainAPI) {
 
     suspend fun load(url: String): Resource<LoadResponse> {
         return safeApiCall {
-            com.lagradost.cloudstream3.CommonActivity.showToast(null, "test", null)
             withTimeout(getTimeout(api.loadTimeoutMs)) {
                 if (isInvalidData(url)) throw ErrorLoadingException()
                 val fixedUrl = api.fixUrl(url)
