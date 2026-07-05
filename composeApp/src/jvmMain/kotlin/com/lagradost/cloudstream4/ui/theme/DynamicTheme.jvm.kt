ca295062cca098c2310399ff42d30bbe97c382f0
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-05 16:32:57 -0600

    -

--- composeApp/src/jvmMain/kotlin/com/lagradost/cloudstream4/ui/theme/DynamicTheme.jvm.kt
+++ composeApp/src/jvmMain/kotlin/com/lagradost/cloudstream4/ui/theme/DynamicTheme.jvm.kt
@@ -2,7 +2,6 @@ package com.lagradost.cloudstream4.ui.theme
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
-import androidx.compose.ui.graphics.Color
 
 @Composable
 @ReadOnlyComposable
