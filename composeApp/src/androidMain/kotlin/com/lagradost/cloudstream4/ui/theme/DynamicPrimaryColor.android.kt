fb5baeac69eb590a14f803dfaab3ce325489b0c1
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-05 15:10:20 -0600

    ReadOnlyComposable

--- composeApp/src/androidMain/kotlin/com/lagradost/cloudstream4/ui/theme/DynamicPrimaryColor.android.kt
+++ composeApp/src/androidMain/kotlin/com/lagradost/cloudstream4/ui/theme/DynamicPrimaryColor.android.kt
@@ -1,11 +1,14 @@
 package com.lagradost.cloudstream4.ui.theme
 
 import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 
 @Composable
+@ReadOnlyComposable
 actual fun resolveDynamicPrimaryColor(): Color {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
         colorResource(android.R.color.system_accent1_200)
@@ -13,6 +16,7 @@ actual fun resolveDynamicPrimaryColor(): Color {
 }
 
 @Composable
+@ReadOnlyComposable
 actual fun resolveDynamicSecondaryColor(): Color {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
         colorResource(android.R.color.system_accent2_200)
