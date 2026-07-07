c2163cef2a11f8200d743c8993df4d8d0356b918
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-07 12:05:43 -0600

    abstract again

--- app/src/main/java/com/lagradost/cloudstream3/utils/DataStoreHelper.kt
+++ app/src/main/java/com/lagradost/cloudstream3/utils/DataStoreHelper.kt
@@ -257,23 +257,31 @@ object DataStoreHelper {
     /**
      * Used to display notifications on new episodes and posters in library.
      */
-    interface LibrarySearchResponse : SearchResponse {
-        val latestUpdatedTime: Long
-        val year: Int?
-        val syncData: Map<String, String>?
-        val plot: String?
-        val tags: List<String>?
-
-        @get:JsonProperty("rating", access = JsonProperty.Access.WRITE_ONLY)
-        @set:JsonProperty("rating", access = JsonProperty.Access.WRITE_ONLY)
+    @Serializable
+    abstract class LibrarySearchResponse(
+        @Transient override var id: Int? = null,
+        @Transient open val latestUpdatedTime: Long = 0L,
+        @Transient override val name: String = "",
+        @Transient override val url: String = "",
+        @Transient override val apiName: String = "",
+        @Transient override var type: TvType? = null,
+        @Transient override var posterUrl: String? = null,
+        @Transient open val year: Int? = null,
+        @Transient open val syncData: Map<String, String>? = null,
+        @Transient override var quality: SearchQuality? = null,
+        @Transient override var posterHeaders: Map<String, String>? = null,
+        @Transient open val plot: String? = null,
+        @Transient override var score: Score? = null,
+        @Transient open val tags: List<String>? = null,
+    ) : SearchResponse {
+        @JsonProperty("rating", access = JsonProperty.Access.WRITE_ONLY)
         @SerialName("rating")
         @Deprecated(
             "`rating` is the old scoring system, use score instead",
             replaceWith = ReplaceWith("score"),
             level = DeprecationLevel.ERROR,
         )
-        var rating: Int?
-            get() = null
+        var rating: Int? = null
             set(value) {
                 if (value != null) {
                     @Suppress("DEPRECATION_ERROR")
@@ -302,10 +310,25 @@ object DataStoreHelper {
         @JsonProperty("plot") @SerialName("plot") override val plot: String? = null,
         @JsonProperty("score") @SerialName("score") override var score: Score? = null,
         @JsonProperty("tags") @SerialName("tags") override val tags: List<String>? = null,
-    ) : LibrarySearchResponse {
+    ) : LibrarySearchResponse(
+        id,
+        latestUpdatedTime,
+        name,
+        url,
+        apiName,
+        type,
+        posterUrl,
+        year,
+        syncData,
+        quality,
+        posterHeaders,
+        plot,
+        score,
+        tags,
+    ) {
         object Serializer : WriteOnlySerializer<SubscribedData>(
             SubscribedData.generatedSerializer(),
-            setOf("rating2"),
+            setOf("rating"),
         )
 
         fun toLibraryItem(): SyncAPI.LibraryItem? {
@@ -350,7 +373,20 @@ object DataStoreHelper {
         @JsonProperty("plot") @SerialName("plot") override val plot: String? = null,
         @JsonProperty("score") @SerialName("score") override var score: Score? = null,
         @JsonProperty("tags") @SerialName("tags") override val tags: List<String>? = null,
-    ) : LibrarySearchResponse {
+    ) : LibrarySearchResponse(
+        id,
+        latestUpdatedTime,
+        name,
+        url,
+        apiName,
+        type,
+        posterUrl,
+        year,
+        syncData,
+        quality,
+        posterHeaders,
+        plot,
+    ) {
         object Serializer : WriteOnlySerializer<BookmarkedData>(
             BookmarkedData.generatedSerializer(),
             setOf("rating"),
@@ -398,7 +434,20 @@ object DataStoreHelper {
         @JsonProperty("plot") @SerialName("plot") override val plot: String? = null,
         @JsonProperty("score") @SerialName("score") override var score: Score? = null,
         @JsonProperty("tags") @SerialName("tags") override val tags: List<String>? = null,
-    ) : LibrarySearchResponse {
+    ) : LibrarySearchResponse(
+        id,
+        latestUpdatedTime,
+        name,
+        url,
+        apiName,
+        type,
+        posterUrl,
+        year,
+        syncData,
+        quality,
+        posterHeaders,
+        plot,
+    ) {
         object Serializer : WriteOnlySerializer<FavoritesData>(
             FavoritesData.generatedSerializer(),
             setOf("rating"),
