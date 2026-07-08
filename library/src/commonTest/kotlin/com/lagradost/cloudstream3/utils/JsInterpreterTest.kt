a6b3b7923f3439212e292246e092a43dbbd457d3
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-08 11:11:42 -0600

    Try

--- library/src/commonTest/kotlin/com/lagradost/cloudstream3/utils/JsInterpreterTest.kt
+++ library/src/commonTest/kotlin/com/lagradost/cloudstream3/utils/JsInterpreterTest.kt
@@ -2183,7 +2183,7 @@ class JsInterpreterTest {
                 withTimeout(300.milliseconds) {
                     val mark = TimeSource.Monotonic.markNow()
                     try {
-                        evalJsInternal("while(true){}", scope = this)
+                        evalJs("while(true){}")
                     } finally {
                         elapsed = mark.elapsedNow()
                     }
