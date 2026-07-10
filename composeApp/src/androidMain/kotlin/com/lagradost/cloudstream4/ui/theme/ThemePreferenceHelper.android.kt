552fa3ffb2c59f5e1928ed03dfdc01a1af4b326a
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-10 15:37:22 -0600

    Update

--- composeApp/src/androidMain/kotlin/com/lagradost/cloudstream4/ui/theme/ThemePreferenceHelper.android.kt
+++ composeApp/src/androidMain/kotlin/com/lagradost/cloudstream4/ui/theme/ThemePreferenceHelper.android.kt
@@ -19,7 +19,7 @@ fun Context.loadThemeMode(): CloudStreamThemeMode {
         "SilentBlue" -> CloudStreamThemeMode.SilentBlue
         "Monet" -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             CloudStreamThemeMode.Dynamic
-        } else CloudStreamThemeMode.Dark
+        } else { CloudStreamThemeMode.Dark }
         else -> CloudStreamThemeMode.Dark
     }
 }
@@ -49,10 +49,10 @@ fun Context.loadPrimaryColor(): CloudStreamPrimaryColor {
         "Lavender" -> CloudStreamPrimaryColor.LAVENDER
         "Monet" -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             CloudStreamPrimaryColor.DYNAMIC
-        } else CloudStreamPrimaryColor.NORMAL
+        } else { CloudStreamPrimaryColor.NORMAL }
         "Monet2" -> if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             CloudStreamPrimaryColor.DYNAMIC_TWO
-        } else CloudStreamPrimaryColor.NORMAL
+        } else { CloudStreamPrimaryColor.NORMAL }
         else -> CloudStreamPrimaryColor.NORMAL
     }
 }
