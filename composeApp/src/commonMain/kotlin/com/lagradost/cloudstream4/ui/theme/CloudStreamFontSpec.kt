c6b5a64f73ba875e9be6819ba76009fb95e3727e
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-15 16:43:09 -0700

    Update

--- composeApp/src/commonMain/kotlin/com/lagradost/cloudstream4/ui/theme/CloudStreamFontSpec.kt
+++ composeApp/src/commonMain/kotlin/com/lagradost/cloudstream4/ui/theme/CloudStreamFontSpec.kt
@@ -2,10 +2,10 @@ package com.lagradost.cloudstream4.ui.theme
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import org.jetbrains.compose.resources.Font
 import org.jetbrains.compose.resources.FontResource
 
 sealed interface CloudStreamFontSpec {
