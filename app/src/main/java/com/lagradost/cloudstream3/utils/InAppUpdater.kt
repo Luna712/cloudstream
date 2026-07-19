6289b2095a8d7abc611df9dcb0c8b89eaee98e95
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-07-19 16:10:18 -0600

    Use kotlinx io

--- app/src/main/java/com/lagradost/cloudstream3/utils/InAppUpdater.kt
+++ app/src/main/java/com/lagradost/cloudstream3/utils/InAppUpdater.kt
@@ -27,11 +27,11 @@ import com.lagradost.cloudstream3.utils.Coroutines.ioSafe
 import com.lagradost.cloudstream3.utils.GitInfo.currentCommitHash
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import kotlinx.io.Sink
+import kotlinx.io.asSink
+import kotlinx.io.buffered
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import okio.BufferedSink
-import okio.buffer
-import okio.sink
 import java.io.BufferedReader
 import java.io.File
 import java.io.IOException
@@ -171,7 +171,7 @@ object InAppUpdater {
             return Update(false, null, null, null, null)
         }
 
-        val tagResponse = parseJson<GithubTag>(app.get(tagUrl, headers = headers).text)
+        val tagResponse = parseJson<GithubTag>(app.get(tagUrl, headers = headers).text())
         val updateCommitHash = tagResponse.githubObject.sha.trim().take(7)
         Log.d(LOG_TAG, "Fetched GitHub tag: $updateCommitHash")
 
@@ -198,11 +198,12 @@ object InAppUpdater {
             }?.forEach { deleteFileOnExit(it) }
 
             val downloadedFile = File.createTempFile(appUpdateName, ".$appUpdateSuffix")
-            val sink: BufferedSink = downloadedFile.sink().buffer()
+            val sink: Sink = downloadedFile.outputStream().asSink().buffered()
 
             updateLock.withLock {
-                sink.writeAll(app.get(url).body().source())
-                sink.close()
+                sink.use {
+                    it.transferFrom(app.get(url).body().source())
+                }
                 openApk(this, Uri.fromFile(downloadedFile))
             }
 
